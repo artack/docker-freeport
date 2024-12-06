e6e0ae0a8d0ece2df1c4c38948292aa0bdb61edb
Patrick Landolt <patrick.landolt@artack.ch>
2024-12-06 17:22:43 +0100

    php84

--- src/Command/FindCommand.php
+++ src/Command/FindCommand.php
@@ -30,7 +30,7 @@ class FindCommand extends Command
         ;
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
         $io->title('Finding a free PORT to use as docker db port');
